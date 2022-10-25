6a187c31daebce09ed9ea57c434ae98886c47179
Sans91 <sansomailjokes@gmail.com>
2022-10-25 15:23:46 +0300

    added square and sqroot features. added comments to code

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,34 +1,81 @@
+square = 0
+n = 0
 numberA = ""
 numberB = ""
-processSymbol = ""
+process = ""
 
+#User makes an input number A, while loop for excepting misinput
 while(type(numberA) != int):
     print("please input number A")
-    try:
+    try:    #trying to catch misinput
         numberA = int(input())
     except:
-        print("you have not entered a number, try again")        
+        print("you have not entered a number, try again")   
 
-while(type(numberB) != int):
-    print("please input number B")
-    try:
-        numberB = int(input())
-    except:
-        print("you have not entered a number, try again")    
+#User makes an input on which process he wants, while loop for excepting misinput
+while(process != "/" and process != "*" and process != "+" and process != "-" and process != "square" and process != "sqroot"):
+    print("please enter process you want\n(available process: /, *, +, -, square, sqroot)")
+    process = input()
 
-while(processSymbol != "/" and processSymbol != "*" and processSymbol != "+" and processSymbol != "-"):
-    print("please enter process you want(example: /, *, +, -)")
-    processSymbol = input()
-    print(type(processSymbol))
-
-match processSymbol:
+#Matching user input
+match process:
     case "/":
-        result = numberA / numberB
+        while(type(numberB) != int):
+            print("please input number B")
+            try:
+                numberB = int(input())
+            except:
+                print("you have not entered a number, try again")  
+        #trying except the zerodivisionproblem
+        try:
+            result = numberA / numberB
+        except:
+            result = "Infinity"
     case "+":
+        while(type(numberB) != int):
+            print("please input number B")
+            try:
+                numberB = int(input())
+            except:
+                print("you have not entered a number, try again") 
         result = numberA + numberB
     case "-":
+        while(type(numberB) != int):
+            print("please input number B")
+            try:
+                numberB = int(input())
+            except:
+                print("you have not entered a number, try again") 
         result = numberA - numberB
     case "*":
-        result = numberA * numberB                     
-
+        while(type(numberB) != int):
+            print("please input number B")
+            try:
+                numberB = int(input())
+            except:
+                print("you have not entered a number, try again") 
+        result = numberA * numberB    
+    case "square":
+        square = numberA * numberA
+    case "sqroot":
+        #finding whole part of a number by finding square higher than square of our number
+        while(square <= numberA):
+            n += 1
+            square = n * n
+        n -= 1 #reverting n to when its not higher than number
+        numberB = n
+        square = 0 #nullifying variable for next calculation
+        #finding .1 part of a number by finding square higher than square of our number
+        while(square <= numberA):  
+            n += 0.1
+            square = n * n
+        numberB = n - 0.1
+        square = 0
+        n -= 0.1
+        #finding .01 part of a number by finding square higher than square of our number
+        while(square <= numberA):
+            n+=0.01
+            square = n * n
+        result = n
+#output of any result that we get
 print(result)
