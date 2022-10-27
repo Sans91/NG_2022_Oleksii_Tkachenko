6ce348c696e40e3423fe7a6c285570215857decb
Sans91 <sansomailjokes@gmail.com>
2022-10-27 22:41:10 +0300

    simplified code and made some minor changes

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -2,30 +2,17 @@ square = 0
 n = 0
 numberA = ""
 numberB = ""
-process = ""
 
-#User makes an input number A, while loop for excepting misinput
-while(type(numberA) != float):
-    print("please input number A")
-    try:    #trying to catch misinput
-        numberA = float(input())
-    except:
-        print("you have not entered a number, try again")   
+#waiting for user input and converting it into a number(float) type
+print("please input number A")
+numberA = float(input(">>"))   
 
-#User makes an input on which process he wants, while loop for excepting misinput
-while(process != "/" and process != "*" and process != "+" and process != "-" and process != "square" and process != "sqroot"):
-    print("please enter process you want\n(available process: /, *, +, -, square, sqroot)")
-    process = input()
-
+print("please enter process you want\n(available process: /, *, +, -, square, sqroot)")
 #Matching user input
-match process:
+match input(">>"):
     case "/":
-        while(type(numberB) != float):
-            print("please input number B")
-            try:
-                numberB = float(input())
-            except:
-                print("you have not entered a number, try again")  
+        print("please input number B")
+        numberB = float(input(">>"))
         #trying except the zerodivisionproblem
         try:
             result = numberA / numberB
@@ -33,30 +20,18 @@ match process:
             result = "Infinity"
     
     case "+":
-        while(type(numberB) != float):
-            print("please input number B")
-            try:
-                numberB = float(input())
-            except:
-                print("you have not entered a number, try again") 
+        print("please input number B")
+        numberB = float(input(">>"))
         result = numberA + numberB
     
     case "-":
-        while(type(numberB) != float):
-            print("please input number B")
-            try:
-                numberB = float(input())
-            except:
-                print("you have not entered a number, try again") 
+        print("please input number B")
+        numberB = float(input(">>"))
         result = numberA - numberB
     
     case "*":
-        while(type(numberB) != float):
-            print("please input number B")
-            try:
-                numberB = float(input())
-            except:
-                print("you have not entered a number, try again") 
+        print("please input number B")
+        numberB = float(input(">>"))
         result = numberA * numberB    
     
     case "square":
@@ -75,10 +50,10 @@ match process:
         while(square <= numberA):  
             n += 0.1
             square = n * n
-        numberB = n - 0.1
-        square = 0
         n -= 0.1
-        
+        numberB = n
+        square = 0
+
         #finding .01 part of a number by finding square higher than square of our number
         while(square <= numberA):
             n+=0.01
