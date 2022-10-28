49108c15fd9debf6ace776721f0454623df5a6d2
Sans91 <sansomailjokes@gmail.com>
2022-10-28 22:50:46 +0300

    deleted useless variables

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,26 +1,24 @@
 
 buffer = ""
 print("input a text to decypher")
-a = input(">>")
-i = 0
-b = [5000]
+text = input(">>")
+
 print("input for how much you want it to shift")
 numberOfShifts = int(input(">>"))
-for element in a:
-    b[i] = element
-    c = ord(b[i])
+for element in text:
+    c = ord(element)
     if c >= 65 and c<= 90:
             c = c + numberOfShifts
             while c > 90:
                 d = c % 90
                 c = 64 + d
-            b[i] = chr(c)  
+            element = chr(c)  
     if c >= 97 and c<= 122:
             c = c + numberOfShifts
             while c >= 123:
                 d = c % 123
                 c = 97 + d
-            b[i] = chr(c)    
-    buffer += b[i]
+            element = chr(c)    
+    buffer += element
     
 print(buffer)
