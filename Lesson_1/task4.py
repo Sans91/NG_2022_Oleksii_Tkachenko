32b7ed6edc6ad3364c4d10e66148cb86abc4f048
Sans91 <sansomailjokes@gmail.com>
2022-10-28 21:46:07 +0300

    made easy programm that calculates unknown numbers in quadratic equation

---
+++ Lesson_1/task4.py
@@ -0,0 +1,19 @@
+import math
+
+
+a = float(input("type number a\n>>"))
+b = float(input("type number b\n>>"))
+c = float(input("type number c\n>>"))
+
+D = b * b - 4 * a * c
+if D > 0:
+    Droot = math.sqrt(D)
+    x1 = (-b + Droot)/(2 * a)
+    x2 = (-b - Droot)/(2 * a)
+    print(f"x1 ={x1},\nx2 = {x2}")
+elif D == 0:
+    Droot = math.sqrt(D)
+    x = -b /  (2 * a)
+    print(f"x = {x}")
+else:
+    print("there are no roots")
