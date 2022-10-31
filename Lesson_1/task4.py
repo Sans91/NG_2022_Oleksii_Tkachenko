dc7bc96af119d6d703d4df26728017446c800f8e
Sans91 <sansomailjokes@gmail.com>
2022-10-31 17:19:04 +0200

    made programm independent from import math

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,4 +1,5 @@
-import math
+Droot = 0
+square = 0
 
 #getting input from user on quadratic equation (ax^2 + bx + c = 0)
 a = float(input("type number a\n>>"))
@@ -9,10 +10,15 @@ c = float(input("type number c\n>>"))
 D = b * b - 4 * a * c
 #if Discriminant is greater than 0, we need to find two variants of x with formula which has Discriminant root
 if D > 0:
-    Droot = math.sqrt(D)
+    #finding root
+    while(square < D):
+        Droot+=0.01       
+        #rounding result for no crazy numbers
+        Droot = round(Droot, 2)
+        square = Droot * Droot
     x1 = (-b + Droot)/(2 * a)
     x2 = (-b - Droot)/(2 * a)
-    print(f"x1 ={x1},\nx2 = {x2}")
+    print(f"x1 = {x1},\nx2 = {x2}")
 #if Discriminant is 0, then there are only one x, which doesn't need root
 elif D == 0:
     x = -b /  (2 * a)
