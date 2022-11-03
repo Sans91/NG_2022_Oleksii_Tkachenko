0bafa3fac9dfbcb06b02510c8ea46902f50540fe
Sans91 <sansomailjokes@gmail.com>
2022-11-03 22:17:42 +0200

    testing the alpha version for sorting dictionary keys

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -3,18 +3,14 @@ dct = {
 }
 arr = []
 string = input("input your string\n>>")
-for char in string:
-    arr.append(ord(char))
-arr.sort()
-for char in arr:
-    print(chr(char), end="")
 
 charSet = set(string)
 print("")
 
 for char in charSet:
     characterCount = string.count(char)
-    dct[char] = characterCount
-    print(f"{dct[char]} = {char} ")            
+    dct[characterCount] = char
+dct.keys.sort()
+print(f"{dct[characterCount]} = {characterCount} ")            
 
     
