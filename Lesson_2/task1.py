02c068ea72a3523b074c946bb69a11799f8d37bc
Sans91 <sansomailjokes@gmail.com>
2022-11-09 23:14:08 +0200

    release version of L_2/t1, in which it is sorting user input by characters and character counts

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,29 +1,14 @@
-dct = {
+characterCountDictionary={}
+#user input
+userText = input("input your string\n>>")
 
-}
-arr = []
-string = input("input your string\n>>")
-
-charSet = set(string)
-print("")
-i = 0
+#using set() to make a set of unique characters 
+charSet = set(userText)
+#for every character in charSet we add a pair of char:count to dictionary
 for char in charSet:
-   characterCount = string.count(char)
-   dct[char] = characterCount
-# for key in sorted(dct.keys()):
-#     print(f"{key} = {dct[key]}", end = ", ")
-print(sorted(dct.items(), key=lambda x: x[0]))
-print("\nsorted by value")
-print(str(sorted(dct.items(), key=lambda x: x[1])))
-
-# for char in charSet:
-#     for value in sorted(dct.values()):
-#         if value == dct[char]:
-#             print(f"{char} = {value}", end = " ")
-#             charSet.remove(char)
-#             break
-#         else:
-#             charSet.remove(char)
-#             charSet.append(char)
-#             break
-
+   characterCountDictionary[char] = userText.count(char)
+   
+#sorting dictionary by keys, using sorted(item, key=), items[0] is key in dictionary.items list
+print(f"sorted by character:\n{sorted(characterCountDictionary.items(), key=lambda items: items[0])}")
+#sorting dictionary by values, using sorted(item, key=), items[1] is value in dictionary.items list
+print(f"sorted by value:\n{sorted(characterCountDictionary.items(), key=lambda items: items[1])}")
