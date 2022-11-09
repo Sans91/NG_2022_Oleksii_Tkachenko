e48cec9bec0ce17aca9ce77b7e10cebefabd2459
Sans91 <sansomailjokes@gmail.com>
2022-11-09 22:50:14 +0200

    fixed a major bug, which led to only around 13 variants except of 25 in Wk/d_d program

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,37 +1,64 @@
 textList = []
 alphabetHigh = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(",")
+alphabetLow = list(map(lambda x: x.lower(), alphabetHigh))
 
 #function that tries to add characters into string in list
 #if there are no way to add character in string in list making string in the list
+#also return a increased by one number of iterations
 def AddNumToArrOfStr(numberOfIter):
     try:
-        textList[numberOfIter] += element
+        textList[numberOfIter] += elementShifted
     except:
-        textList.append(element)
+        textList.append(elementShifted)
     numberOfIter += 1
     return numberOfIter
 
 #waiting for user input and converting the text to uppercase
 userText = input("input a text to decypher\n>>")
-userText = userText.upper()
+
 
 #loop to shift every letter in userText string
 for element in userText:
     numberOfIter = 0
+    
     #if letter in text are present in alphabet set
     if element in alphabetHigh:
+        
         #making it go through 26 variations of text
         while(numberOfIter< len(alphabetHigh)):
+            
             #making shifted numberOfIterbased on index of letter
             #from text + number of iterations
             shiftedNumber = alphabetHigh.index(element) + numberOfIter
+            
+            #making it not exceed the alphabetHigh length
             if shiftedNumber >= len(alphabetHigh):
                 shiftedNumber %= len(alphabetHigh)
-            element = alphabetHigh[shiftedNumber]
+            
+            #assigning shifted letter to shifted element variable
+            elementShifted = alphabetHigh[shiftedNumber]
+
             numberOfIter= AddNumToArrOfStr(numberOfIter)
+    elif element in alphabetLow:
+        while(numberOfIter< len(alphabetHigh)):
+            
+            #making shifted numberOfIterbased on index of letter
+            #from text + number of iterations
+            shiftedNumber = alphabetLow.index(element) + numberOfIter
+            
+            #making it not exceed the alphabetLow length
+            if shiftedNumber >= len(alphabetLow):
+                shiftedNumber %= len(alphabetLow)
+            
+            #assigning shifted letter to shifted element variable
+            elementShifted = alphabetLow[shiftedNumber]
+
+            numberOfIter= AddNumToArrOfStr(numberOfIter)
+    #else if its a letter that doesn't match the alphabet, don't shift it
     else:
         while(numberOfIter< len(alphabetHigh)):
             numberOfIter = AddNumToArrOfStr(numberOfIter)
 
+#printing texts of text list 
 for num in range(len(alphabetHigh)):
     print(f"{num}. {textList[num]}")
