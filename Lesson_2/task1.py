7c7ce57c87600bdb1f7d0a5b72e4687bcbb53d04
Sans91 <sansomailjokes@gmail.com>
2022-11-09 13:59:05 +0200

    testing L_2/t1.py

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -6,11 +6,24 @@ string = input("input your string\n>>")
 
 charSet = set(string)
 print("")
-
+i = 0
 for char in charSet:
     characterCount = string.count(char)
-    dct[characterCount] = char
-dct.keys.sort()
-print(f"{dct[characterCount]} = {characterCount} ")            
+    dct[char] = characterCount
+for key in sorted(dct.keys()):
+    print(f"{key} = {dct[key]}", end = ", ")
 
-    
+print("\nsorted by value")
+print(dct.items()[0])
+# for char in charSet:
+#     for value in sorted(dct.values()):
+#         if value == dct[char]:
+#             print(f"{char} = {value}", end = " ")
+#             charSet.remove(char)
+#             break
+#         else:
+#             charSet.remove(char)
+#             charSet.append(char)
+#             break
+print(sorted(dct.values()))
+print("")
