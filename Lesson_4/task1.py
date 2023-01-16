fb36c4f891f327f136a72829b22b6cd6902407e7
Sans91 <sansomailjokes@gmail.com>
2023-01-16 17:53:42 +0200

    L_4/t1 simplified the programm by removing one variable and renamed file variable

--- Lesson_4/task1.py
+++ Lesson_4/task1.py
@@ -1,14 +1,16 @@
-WriteFile = open('PCchars.txt', "w")
-y = True
+PCcharsFile = open('PCchars.txt', "w")
+import platform, os, psutil
 
+# printing user menu from dictionary
 def userInputMenu():
     alphabet = "abcdefghijklmnopqrstuvwxz"
     alphabetOrder = 0
     for key in choiceDict:
         print(f"{alphabet[alphabetOrder]}){key}: {choiceDict[key]}")
         alphabetOrder += 1
-        
-def choiceFunc(item):
+
+# making yes - no and vice versa        
+def choiceFunc(item):  
     if choiceDict[item] == "no":
         choiceDict[item] = "yes"
     else:
@@ -19,27 +21,25 @@ def choiceWrite():
                 if choiceDict[key] == "yes":
                     match key:
                         case "CPU core count":
-                            WriteFile.write(f"CPU: {psutil.cpu_count()} cores\n")
+                            PCcharsFile.write(f"CPU: {psutil.cpu_count()} cores\n")
                         case "CPU max frequency":
-                            WriteFile.write(f"CPU: {psutil.cpu_freq()[2]} MHz max frequency\n")
+                            PCcharsFile.write(f"CPU: {psutil.cpu_freq()[2]} MHz max frequency\n")
                         case "CPU current frequency":
-                            WriteFile.write(f"CPU: {psutil.cpu_freq()[0]} MHz current frequency\n")
+                            PCcharsFile.write(f"CPU: {psutil.cpu_freq()[0]} MHz current frequency\n")
                         case "CPU family":
-                            WriteFile.write(f"CPU: {platform.processor()}\n")
+                            PCcharsFile.write(f"CPU: {platform.processor()}\n")
                         case "RAM total":
-                            WriteFile.write(f"RAM: {psutil.virtual_memory().total /1024/1024/1024} GB total with OS \n")
+                            PCcharsFile.write(f"RAM: {psutil.virtual_memory().total /1024/1024/1024} GB total with OS \n")
                         case "RAM available":
-                            WriteFile.write(f"RAM: {psutil.virtual_memory().available /1024/1024/1024} GB available to use \n")
+                            PCcharsFile.write(f"RAM: {psutil.virtual_memory().available /1024/1024/1024} GB available to use \n")
                         case "RAM used":
-                            WriteFile.write(f"RAM: {psutil.virtual_memory().used /1024/1024/1024} GB used right now \n") 
+                            PCcharsFile.write(f"RAM: {psutil.virtual_memory().used /1024/1024/1024} GB used right now \n") 
                         case "platform":
-                            WriteFile.write(f"Platform: {platform.platform()}  \n")
+                            PCcharsFile.write(f"Platform: {platform.platform()}  \n")
                         case "architecture":
-                            WriteFile.write(f"Architecture: {platform.architecture()[0]}, {platform.architecture()[1]} \n")  
+                            PCcharsFile.write(f"Architecture: {platform.architecture()[0]}, {platform.architecture()[1]} \n")  
                         case "node":
-                            WriteFile.write(f"Node: {platform.node()}\n")
-import platform, os, socket, sys
-import psutil 
+                            PCcharsFile.write(f"Node: {platform.node()}\n")
 choiceDict = {
     "CPU core count": "no",
     "CPU max frequency": "no",
@@ -51,10 +51,9 @@ choiceDict = {
     "platform": "no",
     "architecture": "no",
     "node": "no"
-
 }
 
-while y != False:
+while 1:
     os.system('cls')
     userInputMenu()
     match input():
@@ -79,8 +78,8 @@ while y != False:
         case "j":
             choiceFunc("node")
         case "y":
-            y = False
             choiceWrite()
+            break
            
-WriteFile.write("Thank you for using this app, more at https://github.com/oleksiy-tkachenko/NG_2022_Oleksii_Tkachenko")
-WriteFile.close()
+PCcharsFile.write("Thank you for using this app, more at https://github.com/oleksiy-tkachenko/NG_2022_Oleksii_Tkachenko")
+PCcharsFile.close()
